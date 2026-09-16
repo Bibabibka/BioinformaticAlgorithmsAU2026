3bd304d85aa6b02b31d9b99bee6b1fb0523f22b6
Racon <raconwinz@gmail.com>
2026-09-16 06:41:51 +0300

    Add reverse complement function

---
+++ HW1/reverse_complement.py
@@ -0,0 +1,8 @@
+def reverse_complement(pattern: str) -> str:
+    s = list(pattern)
+    s = s[::-1]
+    compl = {"A": "T", "T": "A", "G": "C", "C": "G"}
+    s = [compl[i] for i in s]
+    return "".join(s)
+
+
